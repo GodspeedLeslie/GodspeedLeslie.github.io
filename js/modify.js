db3c3c6604174f61eabf6a906f685ea375655803
godspeedleslie <924284131@qq.com>
2020-12-18 10:15:26 +0800

    Site updated: 2020-12-18 10:15:25

--- js/modify.js
+++ js/modify.js
@@ -1,1 +1,8 @@
-"use strict";$(document).ready(function(t){$(".copyright").html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Godspeed')});var app=document.getElementById("footer");app.removeAttribute("id");
+// 设置页脚博主会动的心
+$(document).ready(function(e){
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Godspeed');
+})
+
+// 移除页脚样式
+var app = document.getElementById('footer');
+app.removeAttribute('id')
